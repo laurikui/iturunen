f311b2c4281e2f18712d900acd7aac5c4b5e588b
turuneni <isto.turunen@gmail.com>
2019-10-04 14:26:15 +0300

    Tehty uusi metodi hello word

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -1,4 +1,10 @@
 package fi.academy;
 
 public class Main {
+    public static void main(String[] args) {
+        // write your code here
+    }
+    public static void print(){
+        System.out.println("Hello World!");
+    }
 }
